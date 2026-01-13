445017926cf6879027f2191f1f29b103e68d02b3
ishala <faishalali22@students.amikom.ac.id>
2026-01-13 23:27:34 +0700

    Add report_status_id and report_status to Report schemas

--- app/schemas/report.py
+++ app/schemas/report.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from typing import Optional, List
 
 from app.schemas.location import Location, LocationCreate, LocationUpdate
+from app.schemas.report_status import ReportStatus
 from app.schemas.media import Media
 from app.schemas.user import UserInDB
 
@@ -14,19 +15,20 @@ class ReportBase(BaseModel):
 
 class ReportCreate(ReportBase):
     location: LocationCreate = None
+    report_status_id: Optional[UUID4] = None
 
 
 class ReportUpdate(BaseModel):
     category: Optional[str] = None
     description: Optional[str] = None
     location: Optional[LocationUpdate] = None
+    report_status_id: Optional[UUID4] = None
 
 
 class ReportInDB(ReportBase):
     id: UUID4
     user_id: UUID4
     location_id: UUID4
-    # status_id removed
     created_at: datetime
 
     class Config:
@@ -35,6 +37,7 @@ class ReportInDB(ReportBase):
 
 class Report(ReportInDB):
     location: Optional[Location] = None
+    report_status: Optional[ReportStatus] = None 
     media: List[Media] = Field(default_factory=list)
 
 # Admin
