bb26f80be3a350aaa3c74ba5f1b9b55eca49206b
ishala <faishalali22@students.amikom.ac.id>
2026-01-16 13:55:01 +0700

    Add verifications endpoint with CRUD operations and authorization checks

---
+++ app/api/v1/endpoints/verifications.py
@@ -0,0 +1,106 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from typing import List, Optional
+from uuid import UUID, uuid4
+
+from app.dependencies import get_db
+from app.crud import (
+    crud_verification, 
+    crud_report_status,
+    crud_report)
+from app.schemas.user import User
+from app.schemas.verification import (
+    Verification,
+    VerificationCreate,
+    VerificationUpdate
+)
+from app.core.security import (
+    check_resource_ownership,
+    get_current_active_admin
+)
+
+router = APIRouter()
+
+@router.get("/", response_model=List[Verification])
+def read_verifications(
+    skip: int = 0,
+    limit: int = 100,
+    db: Session = Depends(get_db),
+    valid_user: User = Depends(get_current_active_admin)
+):
+    if valid_user is not None:
+        verifications = crud_verification.get_verifications(
+            db=db, skip=skip, limit=limit
+        )
+        return verifications
+    else:
+        return None
+
+@router.get("/{verification_id}", response_model=Verification)
+def read_verification(
+    verification_id: UUID,
+    db: Session = Depends(get_db),
+    valid_user: User = Depends(get_current_active_admin)
+):
+    if valid_user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User not allowed to access"
+        )
+    
+    db_verification = crud_verification.get_verification(
+        db=db, verification_id=verification_id
+    )
+    if db_verification is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Verification not found"
+        )
+    return db_verification
+
+@router.post("/", response_model=Verification,
+             status_code=status.HTTP_201_CREATED)
+def create_verification(
+    verification: VerificationCreate,
+    db: Session = Depends(get_db),
+    valid_user: User = Depends(get_current_active_admin),
+):
+    if valid_user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User not allowed to access"
+        )
+    
+    report_status = verification.report_status
+    
+    db_verification = crud_verification.create_verification(
+        db=db,
+        verification=verification,
+        report_id=verification.report_id,
+        admin_id=valid_user.id
+    )
+    
+    if db_verification is not None and report_status != "Pending":
+        new_report_status_id = crud_report_status.get_status_id_by_name(
+            db=db, name=report_status)
+
+        db_report = crud_report.get_report(
+            db=db, report_id=verification.report_id)
+
+        if db_report is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Report not found"
+            )
+
+        db_report.report_status_id = new_report_status_id
+
+        db_report = crud_report.update_report(
+            db=db,
+            report_id=verification.report_id,
+            report=db_report
+        )
+    return db_verification
+
+# @router.patch("/{verification_id}", response_model=Verification)
+# def update_verification
